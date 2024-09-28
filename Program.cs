ffa8c705c6da9409f5da47785ca0ec49e686fd6b
Victoria Tonini <tonini.v.r@outlook.com>
2024-09-28 01:46:34 -0300

    primeiro commit , adicionando complementos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone cel1 = new Nokia(numero: "11995631052", modelo: "a350", iMEI: "123456", memoria: 256);
+cel1.Ligar();
+cel1.ReceberLigacao();
+cel1.InstalarAplicativo("Telegram");
+
+Console.WriteLine("----------------------------------------------------------------");
+Console.WriteLine("Smartphone Iphone");
+Smartphone cel2 = new Iphone(numero: "12985637456", modelo: "16", iMEI: "456789", memoria: 128) ;
+cel2.Ligar();
+cel2.ReceberLigacao();
+cel2.InstalarAplicativo("Instagram");
